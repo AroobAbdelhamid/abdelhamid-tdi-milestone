599a06ff00cd78e70ef9ee92d989b882f22c5220
Ruby Abdelhamid <aroobabdelha@gmail.com>
2021-06-22 16:40:09 -0400

    removed boto for now. need to get this looking better

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, redirect
 #from dotenv import load_dotenv
 # import boto3
-from boto3.s3.connection import S3Connection #can't use dotenv in heroku....
+#import boto
+#from boto3.s3.connection import S3Connection #can't use dotenv in heroku....
 import os
 import re #string parsing
 import requests #to download html data
@@ -53,7 +54,9 @@ def get_url():
     print("hello")
 #    load_dotenv()
 #    API_KEY = os.environ['MY_API_KEY']
-    API_KEY =  S3Connection(os.environ['MY_API_KEY']) #'4PJK6E44KAP57MW0'
+    conn = boto.connect_S3()
+    API_KEY = '4PJK6E44KAP57MW0'# S3Connection(os.environ['MY_API_KEY']) #'4PJK6E44KAP57MW0'
+    #API_KEY =  conn(os.environ['MY_API_KEY']) #'4PJK6E44KAP57MW0'
     if request.method == 'POST':
         stock = request.form.get("stock_tick")
         url_nm = ("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" +
